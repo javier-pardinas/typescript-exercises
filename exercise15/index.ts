2455322e8f1ff4c6cac0a0f3d3038007c6678a41
javierpb275 <javierpb275@gmail.com>
2021-06-27 12:30:53 +0200

    exercise 15 done

--- exercise15/index.ts
+++ exercise15/index.ts
@@ -27,26 +27,26 @@ Exercise:
 
 */
 
-Object
-export class ObjectManipulator {
+type ObjectWithNewProp<T, K extends string, V> = T & {[NK in K]: V};
 
-    constructor(protected obj) {}
+export class ObjectManipulator<T> {
+    constructor(protected obj: T) {}
 
-    public set(key, value) {
-        return new ObjectManipulator({...this.obj, [key]: value});
+    public set<K extends string, V>(key: K, value: V): ObjectManipulator<ObjectWithNewProp<T, K, V>> {
+        return new ObjectManipulator({...this.obj, [key]: value} as ObjectWithNewProp<T, K, V>);
     }
 
-    public get(key) {
+    public get<K extends keyof T>(key: K): T[K] {
         return this.obj[key];
     }
 
-    public delete(key) {
+    public delete<K extends keyof T>(key: K): ObjectManipulator<Omit<T, K>> {
         const newObj = {...this.obj};
         delete newObj[key];
         return new ObjectManipulator(newObj);
     }
 
-    public getObject() {
+    public getObject(): T {
         return this.obj;
     }
 }
